35450c658e5229264a8d0f56ee2d9f53af95d723
ashifkkt <ashif@texolworld.com>
2024-08-13 09:12:27 +0530

    upto-router-settings

---
+++ react-job-finder/src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
+import MainLayout from "./layouts/MainLayout";
+import NotFoundPage from "./pages/NotFoundPage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout/>}>
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+      <Route path="*" element={<NotFoundPage/>}/>
+      </Route>)
+);
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
